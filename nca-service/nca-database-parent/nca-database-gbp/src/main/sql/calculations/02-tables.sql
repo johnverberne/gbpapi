77b639de52927a8fbef82c452d62b76606046bc6
John Verberne <john.verberne@gmail.com>
2019-03-07 09:17:21 +0100

    add calculation result view and test data

--- nca-service/nca-database-parent/nca-database-gbp/src/main/sql/calculations/02-tables.sql
+++ nca-service/nca-database-parent/nca-database-gbp/src/main/sql/calculations/02-tables.sql
@@ -23,8 +23,28 @@ CREATE TABLE calculations
 CREATE TABLE calculation_results
 (
 	calculation_id integer NOT NULL,
+	model text,
+	geolayer text,
 	data jsonb
 );
+
+
+/*
+ * job_calculations
+ * ----------------
+ * Koppeltabel (1:N) tussen berekeningen en de job waar deze berekening bijhoren.
+ * Een job kan dus meerdere berekeningen aan zich gekoppeld hebben. In praktijk maximaal 2: de 'current' en 'proposed' situatie.
+*/
+CREATE TABLE job_calculations (
+	job_id integer NOT NULL,
+	calculation_id integer NOT NULL,
+
+	CONSTRAINT job_calculations_pkey PRIMARY KEY (job_id, calculation_id),	
+	CONSTRAINT job_calculations_fkey_jobs FOREIGN KEY (job_id) REFERENCES jobs,
+	CONSTRAINT job_calculations_fkey_calculations FOREIGN KEY (calculation_id) REFERENCES calculations
+);
+
+CREATE UNIQUE INDEX idx_job_calculations_job_calculation_id ON job_calculations(calculation_id);
 
 
 
