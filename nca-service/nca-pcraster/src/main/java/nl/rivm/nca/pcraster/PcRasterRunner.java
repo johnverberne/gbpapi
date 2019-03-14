b46002b3a36387d2d0c715cca298682eaf730d1c
John Verberne <john.verberne@gmail.com>
2019-03-14 08:02:08 +0100

    add batch file for windows and change the model to reflect the model nkmodel add default to eco_system_service

--- nca-service/nca-pcraster/src/main/java/nl/rivm/nca/pcraster/PcRasterRunner.java
+++ nca-service/nca-pcraster/src/main/java/nl/rivm/nca/pcraster/PcRasterRunner.java
@@ -8,13 +8,16 @@ import org.slf4j.LoggerFactory;
 
 import nl.rivm.nca.runner.Exec;
 import nl.rivm.nca.runner.ExecParameters;
+import nl.rivm.nca.runner.OSUtils;
 
 class PcRasterRunner {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(PcRasterRunner.class);
 
+    // TODO we want a version in the executed string 
 	private static final String PYTHON = "python";
 	private static final String NCA = "/opt/nkmodel/nca.sh";
+	private static final String NCA_WIN = "/opt/nkmodel/nca.bat";
 
 	public void sanityCheck() throws IOException, InterruptedException {
 		final String[] args = { "-c", "\"import pcraster\"" };
@@ -25,11 +28,12 @@ class PcRasterRunner {
 
 	public void runPcRaster(String correlationId, String ecoSystemService, File projectFile)
 			throws IOException, InterruptedException {
+		final String RUNNER = OSUtils.isWindows() ? NCA_WIN : NCA;
 		final String[] args = { ecoSystemService, projectFile.getAbsolutePath() };
-		final ExecParameters execParams = new ExecParameters(NCA, args);
+		final ExecParameters execParams = new ExecParameters(RUNNER, args);
 		final Exec exec = new Exec(execParams, "");
 
-		LOGGER.debug("Execute: ({}) with parameters: {}", NCA, args);
+		LOGGER.debug("Execute: ({}) with parameters: {}", RUNNER, args);
 		exec.run(correlationId, new File(projectFile.getParent()));
 	}
 }
