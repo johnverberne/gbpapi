37a34a23fee37de9ca1040ec7d43ac48a31acbde
John Verberne <john.verberne@gmail.com>
2019-03-27 10:50:44 +0100

    create testgrid for utrecht check build scripts for database

--- nca-service/nca-database-parent/nca-database-gbp/src/data/sql/test_data/02_calculation_results.sql
+++ nca-service/nca-database-parent/nca-database-gbp/src/data/sql/test_data/02_calculation_results.sql
@@ -1,10 +1,9 @@
 -- insert test data for one calculation with 2 scenarios
 
 INSERT INTO jobs (job_id,key,name,state,type,user_id) 
-VALUES (1,'1', 'bomen maatregel scenario', 'completed'::job_state_type, 'calculation'::job_type,1);
+VALUES (1,'1', 'bomen maatregel', 'completed'::job_state_type, 'calculation'::job_type,1);
 	
-
--- scanrio 1
+-- scenario 1
 INSERT INTO calculations (calculation_id, calculation_uuid, state) 
 VALUES (1,'1','completed'::calculation_state_type);
 
@@ -32,7 +31,5 @@ INSERT INTO calculation_results (calculation_id, model, geolayer, data) values (
 
 INSERT INTO calculation_results (calculation_id, model, geolayer, data) values (2, 'air_regulation', 'TEEB_Minder_gezondheidskosten_door_afvang_fijn_stof.tiff',
 '{"legendrgbmin": "ff0000", "code": 1002, "name": "TEEB_Minder_gezondheidskosten_door_afvang_fijn_stof", "min": 3.132479429244995, "geobox": {"xmin": 140800.0, "ymin": 458580.0, "ymax": 459500.0, "xmax": 142170.0}, "max": 533.2514038085938, "sum": 1704185.5, "legendrgbmax": "00ff00", "legendmin": 0, "legendmax": 100, "units": "Euro", "model": "air_regulation", "avg": 135.20989990234375, "class": "monetary"}');
-
-
 
 
