53274bffdcb4036b39f878b40f4566e6fe64639b
John Verberne <john.verberne@gmail.com>
2019-04-12 09:31:04 +0200

    make 5 parameters 3 file path reference and 2 string paramenters

--- nca-service/nca-pcraster/src/main/java/nl/rivm/nca/pcraster/PreProcessRunner.java
+++ nca-service/nca-pcraster/src/main/java/nl/rivm/nca/pcraster/PreProcessRunner.java
@@ -11,32 +11,42 @@ import nl.rivm.nca.runner.ExecParameters;
 import nl.rivm.nca.runner.OSUtils;
 
 class PreProcessRunner {
-  
-  private static final Logger LOGGER = LoggerFactory.getLogger(PreProcessRunner.class);
-  
-    // TODO we want a version in the executed string parsed from the api
-	private static final String NCA_PREPROCESS = "/opt/nkmodel/nca_preprocess_scenario_map.sh"; //{NKMODEL_PATH}/bin/nca_preprocess_scenario_map.sh
-	private static final String NCA_WIN_PREPROCESS = "/opt/nkmodel/nca_preprocess_scenario_map.bat"; //{NKMODEL_PATH}/nca_preprocess_scenario_map.bat
 
-	/*
-	 * create a new map (edit_map) file from current map file mutated with the values from xyz file.
+	private static final Logger LOGGER = LoggerFactory.getLogger(PreProcessRunner.class);
+
+	// TODO we want a version in the executed string parsed from the api
+	private static final String NCA_PREPROCESS = "/opt/nkmodel/nca_preprocess_scenario_map.sh"; // {NKMODEL_PATH}/bin/nca_preprocess_scenario_map.sh
+	private static final String NCA_WIN_PREPROCESS = "/opt/nkmodel/nca_preprocess_scenario_map.bat"; // {NKMODEL_PATH}/nca_preprocess_scenario_map.bat
+
+	/**
+	 * create a new map (edit_map) file from current map file mutated with the
+	 * values from xyz file.
 	 * 
-	 * input xyz
-	 * output map
-	 * output edit_map
+	 * @param correlationId
+	 * @param xyzFile 
+	 * @param mapFile
+	 * @throws IOException
+	 * @throws InterruptedException
 	 */
 	public void runPreProcessor(String correlationId, File xyzFile, File mapFile)
 			throws IOException, InterruptedException {
 		final String RUNNER = OSUtils.isWindows() ? NCA_WIN_PREPROCESS : NCA_PREPROCESS;
 		String xyzFilePath = xyzFile.getAbsolutePath();
-		String editMapFilePath = new File(mapFile.getAbsolutePath().replace(".map", "_edit.map")).getAbsolutePath();
 		String mapFilePath = mapFile.getAbsolutePath();
-		String pcrCalcParameter = "\""+ mapFilePath + "=cover\\(" + mapFilePath + "," + editMapFilePath + "\\)\""; 
-		final String[] args = {xyzFilePath,  editMapFilePath, mapFilePath, pcrCalcParameter};
+		// new file will be created in batch file
+		String editMapFilePath = new File(mapFile.getAbsolutePath().replace(".map", "_edit.map")).getAbsolutePath();
+		// parse paramenters as string
+		String mapFileParameter = paramString(mapFilePath.replaceAll("/", "\\"));
+		String editMapFileParameter = paramString(editMapFilePath.replaceAll("/", "\\"));
+		final String[] args = { xyzFilePath, mapFilePath, mapFileParameter, editMapFileParameter };
 		final ExecParameters execParams = new ExecParameters(RUNNER, args);
 		final Exec exec = new Exec(execParams, "", false); // run as batch file
 
 		LOGGER.debug("Execute: ({}) with parameters: {}", RUNNER, args);
-		exec.run(correlationId, new File(xyzFile.getParent())); //new File(projectFileScenario.getParent())
+		exec.run(correlationId, new File(xyzFile.getParent()));
+	}
+
+	private String paramString(String parma) {
+		return "\"" + parma + "\"";
 	}
 }
