2e4fb38ab0bd13636b3766c459a88e0926c463ee
Geert Tasseron <trg@dat.nl>
2019-04-25 11:36:36 +0200

    Let sidebar react to navigation changes that have occured not only by clicking in the menu.

--- nca-webapp/src/app/components/sidebar/sidebar-component.ts
+++ nca-webapp/src/app/components/sidebar/sidebar-component.ts
@@ -1,32 +1,40 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { MessageEventService } from '../../services/message-event-service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd, RouterEvent } from '@angular/router';
 import { CurrentProjectService } from '../../services/current-project-service';
 import { MenuEventService } from '../../services/menu-event-service';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'gbp-sidebar',
   templateUrl: './sidebar-component.html',
   styleUrls: ['./sidebar-component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
-
-  @Output() public activeMenu: string = 'REFERENCE';
+  public activeMenu: string = 'REFERENCE';
+  private navigation: Observable<NavigationEnd>;
 
   constructor(
+    public projectService: CurrentProjectService,
     private translateService: TranslateService,
     private messageService: MessageEventService,
     private router: Router,
-    public projectService: CurrentProjectService,
     private menuService: MenuEventService) {
+      this.navigation = router.events.pipe(
+        filter(evt => evt instanceof NavigationEnd)
+      ) as Observable<NavigationEnd>;
+  }
+
+  public ngOnInit() {
+    this.navigation.subscribe(evt => this.navigated(evt));
   }
 
   public onMenuClick(event: string) {
     if (this.activeMenu !== event) {
       this.menuService.mainMenuChange();
-      this.activeMenu = event;
       if (event === 'LAYERS' || event === 'SETTINGS') {
         this.messageService.sendMessage('WIP');
         this.router.navigate(['dummy']);
@@ -34,6 +42,11 @@ export class SidebarComponent {
         this.router.navigate([{ outlets: { primary: event.toLowerCase(), main: 'map' }}]);
       }
     }
+  }
+
+  private navigated(event: RouterEvent ) {
+    const endIndex = event.url.indexOf('(');
+    this.activeMenu = event.url.substring(1, endIndex).toLocaleUpperCase();
   }
 
 }
