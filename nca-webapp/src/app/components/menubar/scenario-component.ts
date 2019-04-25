fd3e54294677be12b794ad1292d33bae5dfb94cb
Geert Tasseron <trg@dat.nl>
2019-04-25 13:19:18 +0200

    Warn user when measure extent is large

--- nca-webapp/src/app/components/menubar/scenario-component.ts
+++ nca-webapp/src/app/components/menubar/scenario-component.ts
@@ -5,6 +5,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { MeasureComponent } from './measure-component';
 import { TranslateService } from '@ngx-translate/core';
 import { ProjectModel } from '../../models/project-model';
+import { GridCellModel } from '../../models/grid-cell-model';
+import { MessageEventService } from '../../services/message-event-service';
 
 @Component({
   selector: 'gbp-scenario',
@@ -16,11 +18,13 @@ export class ScenarioComponent implements OnChanges {
   @ViewChild(MeasureComponent) private gbpMeasures: MeasureComponent;
   @Input() public scenarioModel: ScenarioModel;
   public scenarioForm: FormGroup;
+  private EXTENT_THRESHOLD: number = 10000000;
 
   constructor(private fb: FormBuilder,
     public cdRef: ChangeDetectorRef,
     public projectService: CurrentProjectService,
-    private translateService: TranslateService) {
+    private translateService: TranslateService,
+    private messageService: MessageEventService) {
     this.scenarioForm = this.constructForm(this.fb);
   }
 
@@ -57,6 +61,7 @@ export class ScenarioComponent implements OnChanges {
       this.scenarioModel.measures = this.gbpMeasures.saveMeasures();
       if (this.scenarioModel.measures) {
         this.scenarioModel.valid = true;
+        this.checkMeasureExtent();
       }
     }
     console.log(JSON.stringify(this.projectService.currentProject as ProjectModel));
@@ -69,6 +74,28 @@ export class ScenarioComponent implements OnChanges {
 
   public hasMeasures() {
     return this.gbpMeasures.measures.length > 0;
+  }
+
+  private checkMeasureExtent() {
+    for (const measure of this.scenarioModel.measures) {
+      measure.geom.cells.sort(this.compare);
+      const start = measure.geom.cells[0].coordsAfrt;
+      const end = measure.geom.cells[measure.geom.cells.length - 1].coordsAfrt;
+      if (this.getExtentSize(start, end) > this.EXTENT_THRESHOLD) {
+        this.messageService.sendMessage('WARNING_LARGE_EXTENT_CALCULATION');
+        break;
+      }
+    }
+  }
+
+  private compare(a: GridCellModel, b: GridCellModel) {
+    return a.coordsAfrt[1] === b.coordsAfrt[1] ? a.coordsAfrt[0] - b.coordsAfrt[0] : a.coordsAfrt[1] - b.coordsAfrt[1];
+  }
+
+  private getExtentSize(start: any, end: any): number {
+    const xSize = start[0] - end[0];
+    const ySize = start[1] - end[1];
+    return Math.abs(xSize) * Math.abs(ySize);
   }
 
 }
