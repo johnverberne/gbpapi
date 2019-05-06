7e8d5acdf4a491dee55fc2ad9d6f382c67e1eb21
John Verberne <john.verberne@gmail.com>
2019-05-06 13:48:22 +0200

    new endpoint set

--- nca-webapp/src/environments/environment.demo.ts
+++ nca-webapp/src/environments/environment.demo.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ENDPOINT: 'http://131.224.198.105:8080/api/1',
-  GEOSERVER_ENDPOINT: 'http://131.224.198.105:8080/geoserver-gbp/'
+  ENDPOINT: 'http://84.107.68.251:8080/api/1',
+  GEOSERVER_ENDPOINT: 'http://84.107.68.251:8080/geoserver-gbp/'
 };
