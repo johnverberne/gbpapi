41f404c5bec192eec9c20e1c1ea895cd18a14e77
John Verberne <john.verberne@gmail.com>
2019-05-07 13:51:47 +0200

    remove / from environment and add to the request url, add uuid to result response

--- nca-webapp/src/environments/environment.demo.ts
+++ nca-webapp/src/environments/environment.demo.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ENDPOINT: 'http://84.107.68.251:8080/api/1',
-  GEOSERVER_ENDPOINT: 'http://84.107.68.251:8080/geoserver-gbp/'
+  GEOSERVER_ENDPOINT: 'http://84.107.68.251:8080/geoserver-gbp'
 };
